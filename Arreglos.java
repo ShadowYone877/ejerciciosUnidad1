563ccb33bf58f6bc7f4f307ee43a75c22f240d25
brunogaelav2004@gmail.com <brunogaelav2004@gmail.com>
2024-09-13 18:11:48 -0600

    Arreglos

---
+++ Arreglos.java
@@ -0,0 +1,10 @@
+class Arreglos {
+    public static void main(String[] args) {
+        int[] numeros = {1,2,3,4,5};
+
+        System.out.println("---Iterando con un bucle for---");
+        for (int i=0; i<numeros.length; i++) {
+            System.out.println("Valores:" + numeros[i]);
+        }
+    }
+}
